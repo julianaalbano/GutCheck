5641bedd4177116b42dd6aecc8926c375d2eeccd
julianaalbano <julianaalbano.us@gmail.com>
2019-08-10 16:55:22 -0400

    tons of updates

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,97 +1,107 @@
-let socket = io();
-
-function scrollToBottom() {
-  let messages = document.querySelector('#messages').lastElementChild;
-  messages.scrollIntoView();
-}
+let socket = io()
 
 socket.on('connect', function() {
-  let searchQuery = window.location.search.substring(1);
-  let params = JSON.parse('{"' + decodeURI(searchQuery).replace(/&/g, '","').replace(/\+/g, ' ').replace(/=/g,'":"') + '"}');
+  let searchQuery = window.location.search.substring(1)
+  let params = JSON.parse(
+    '{"' +
+      decodeURI(searchQuery)
+        .replace(/&/g, '","')
+        .replace(/\+/g, ' ')
+        .replace(/=/g, '":"') +
+      '"}'
+  )
 
   socket.emit('join', params, function(err) {
-    if(err){
-      alert(err);
-      window.location.href = '/';
-    }else {
-      console.log('No Error');
+    if (err) {
+      alert(err)
+      window.location.href = '/'
+    } else {
+      console.log('No Error')
     }
   })
-});
+})
 
 socket.on('disconnect', function() {
-  console.log('disconnected from server.');
-});
+  console.log('disconnected from server.')
+})
 
-socket.on('updateUsersList', function (users) {
-  let ol = document.createElement('ol');
+socket.on('updateUsersList', function(users) {
+  let ol = document.createElement('ol')
 
-  users.forEach(function (user) {
-    let li = document.createElement('li');
-    li.innerHTML = user;
-    ol.appendChild(li);
-  });
+  users.forEach(function(user) {
+    let li = document.createElement('li')
+    li.innerHTML = user
+    ol.appendChild(li)
+  })
 
-  let usersList = document.querySelector('#users');
-  usersList.innerHTML = "";
-  usersList.appendChild(ol);
+  let usersList = document.querySelector('#users')
+  usersList.innerHTML = ''
+  usersList.appendChild(ol)
 })
 
 socket.on('newMessage', function(message) {
-  const formattedTime = moment(message.createdAt).format('LT');
-  const template = document.querySelector('#message-template').innerHTML;
-  const html = Mustache.render(template, {
-    from: message.from,
-    text: message.text,
-    createdAt: formattedTime
-  });
+  const formattedTime = moment(message.createdAt).format('LT')
+  const from = message.from
+  const text = message.text
 
-  const div = document.createElement('div');
-  div.innerHTML = html
-
-  document.querySelector('#messages').appendChild(div);
-  scrollToBottom();
-});
-
-socket.on('newLocationMessage', function(message) {
-  const formattedTime = moment(message.createdAt).format('LT');
-  console.log("newLocationMessage", message);
-
-  const template = document.querySelector('#location-message-template').innerHTML;
-  const html = Mustache.render(template, {
-    from: message.from,
-    url: message.url,
-    createdAt: formattedTime
-  });
-
-  const div = document.createElement('div');
-  div.innerHTML = html
-
-  document.querySelector('#messages').appendChild(div);
-  scrollToBottom();
-});
-
-document.querySelector('#submit-btn').addEventListener('click', function(e) {
-  e.preventDefault();
-
-  socket.emit("createMessage", {
-    text: document.querySelector('input[name="message"]').value
-  }, function() {
-    document.querySelector('input[name="message"]').value = '';
-  })
+  const p = document.createElement('p')
+  p.innerHTML = `${formattedTime}     |     ${from}: ${text}`
+  document.querySelector('#messages').appendChild(p)
 })
 
-document.querySelector('#send-location').addEventListener('click', function(e) {
-  if (!navigator.geolocation) {
-    return alert('Geolocation is not supported by your browser.')
-  }
+document
+  .querySelector('#submit-btn')
+  .addEventListener('click', function(event) {
+    event.preventDefault()
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-    socket.emit('createLocationMessage', {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    })
-  }, function() {
-    alert('Unable to fetch location.')
+    socket.emit(
+      'createMessage',
+      {
+        text: document.querySelector('input[name="message"]').value
+      },
+      function() {
+        document.querySelector('input[name="message"]').value = ''
+      }
+    )
   })
-});
+
+document.querySelector('#good-btn').addEventListener('click', function(event) {
+  event.preventDefault()
+  socket.emit(
+    'createMessage',
+    {
+      text: 'feels GOOD'
+    },
+    function() {
+      'feedback submitted'
+    }
+  )
+})
+
+document.querySelector('#okay-btn').addEventListener('click', function(event) {
+  event.preventDefault()
+  socket.emit(
+    'createMessage',
+    {
+      text: 'feels OKAY'
+    },
+    function() {
+      'feedback submitted'
+    }
+  )
+})
+
+document
+  .querySelector('#confused-btn')
+  .addEventListener('click', function(event) {
+    event.preventDefault()
+    socket.emit(
+      'createMessage',
+      {
+        text: 'feels CONFUSED'
+      },
+      function() {
+        'feedback submitted'
+      }
+    )
+  })
